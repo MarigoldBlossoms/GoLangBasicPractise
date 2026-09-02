939ed47af6a42dd27201a3854f5f187591efde19
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-09-02 17:13:49 +1200

    Exercise 7 completed

---
+++ 100-golang-exercises-main/007/007.go
@@ -0,0 +1,29 @@
+package practice
+
+/*
+Exercise 007:
+
+Given two integers X and Y, generate a 2-dimensional slice where the element
+at row i and column j equals i*j (with i in [0, X) and j in [0, Y)).
+
+Example: Ex007(3, 5) -> [[0,0,0,0,0], [0,1,2,3,4], [0,2,4,6,8]]
+
+Tip: run `go test ./...` from this folder.
+*/
+
+// Ex007 should build the X-by-Y multiplication matrix as a [][]int.
+func Ex007(x, y int) [][]int {
+	outputs := make([][]int, x)
+
+	for row, _ := range outputs {
+		outputs[row] = make([]int, y)
+	}
+
+	for i, _ := range outputs {
+		for j, _ := range outputs[i] {
+			outputs[i][j] = i * j
+		}
+	}
+
+	return outputs
+}
