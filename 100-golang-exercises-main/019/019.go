7d359d3effb5553230d6e53a8439357989dff903
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-09-03 11:31:42 +1200

    Exercise 19 completed

---
+++ 100-golang-exercises-main/019/019.go
@@ -0,0 +1,30 @@
+package practice
+
+import "sort"
+
+/*
+Exercise 019:
+
+Sort "name,age,score" lines by name, then age, then score (string comparison
+on each field, like the original Python challenge).
+
+Example:
+  Ex019([]string{
+    "Tom,19,80", "John,20,90", "Jony,17,91", "Jony,17,93", "Json,21,85",
+  })
+  ->
+  []string{
+    "John,20,90", "Jony,17,91", "Jony,17,93", "Json,21,85", "Tom,19,80",
+  }
+
+Tip: run `go test ./...` from this folder.
+*/
+
+// Ex019 should return a sorted copy of input.
+func Ex019(input []string) []string {
+
+	sort.SliceStable(input, func(i, j int) bool {
+		return input[i] < input[j]
+	})
+	return input
+}
