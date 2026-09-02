acd33540a537aa74f00e42ed03d0e2a0755939f1
EnchantedSorrow <58747935+EnchantedSorrow@users.noreply.github.com>
2026-09-03 11:40:27 +1200

    Exercise 20 partially completed

---
+++ 100-golang-exercises-main/020/exercise020.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+Exercise 020:
+
+Iterate over the numbers divisible by 7 between 0 and n (exclusive).
+Implement two flavours:
+
+  - Ex020(n) returns them as a slice (easy to test).
+  - Ex020Stream(n) yields them over a channel (Go's closest equivalent to a
+    Python generator using `yield`).
+
+Example:
+  Ex020(20) -> [0, 7, 14]
+
+Tip: run `go test ./...` from this folder.
+*/
+
+// Ex020 should return all non-negative integers < n that are divisible by 7.
+func Ex020(n int) []int {
+	var output []int
+
+	for i := 0; i < n; i++ {
+		if i%7 == 0 {
+			output = append(output, i)
+		}
+	}
+	return output
+}
+
+// Ex020Stream should send the same numbers as Ex020 over a channel, then close it.
+func Ex020Stream(n int) <-chan int {
+	ch := make(chan int)
+	// TODO: send divisible-by-7 numbers from a goroutine and close(ch).
+	defer close(ch)
+	return ch
+}
